6fd78bc216a2be91af4967dc64428fe9395a0f62
Idan Vork <ivork@f1r1s13.codam.nl>
2021-10-13 17:43:08 +0200

    added checker function

---
+++ src/helper1.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   helper1.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ivork <ivork@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/10/13 14:39:47 by ivork         #+#    #+#                 */
+/*   Updated: 2021/10/13 17:26:07 by ivork         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "../includes/push_swap.h"
+#include "../libft/libft.h"
+
+void	check_duplicates(t_stack *stack_a)
+{
+	t_stack *current;
+
+	while (stack_a)
+	{
+		current = stack_a->next;
+		while (current)
+		{
+			if (current->num == stack_a->num)
+				err_func();
+			current = current->next;
+		}
+		stack_a = stack_a->next;
+	}
+}
+
+int	check_argument(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str[i])
+		return (0);
+	if (str[i] == '-' && str[i + 1] != '\0')
+		i++;
+	while (*str == '0')
+		str++;
+	while (str[i] != '\0')
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	if (i > 9 && ft_atoi(str) == 0)
+		return (0);
+	return (1);
+}
+
+t_stack	*create_new_node(void)
+{
+	t_stack	*first;
+
+	first = (t_stack *)malloc(sizeof(t_stack));
+	first->next = NULL;
+	return (first);
+}
+
+t_stack	*fill_stack(int argc, char **argv, t_stack *stack_a)
+{
+	t_stack	*tmp;
+	int		i;
+
+	i = 1;
+	tmp = stack_a;
+	while (i < argc)
+	{
+		if (!check_argument(argv[i]))
+			return (NULL);
+		tmp->num = ft_atoi(argv[i]);
+		tmp->next = NULL;
+		if (i < argc - 1)
+		{
+			tmp->next = create_new_node();
+			tmp = tmp->next;
+		}
+		i++;
+	}
+	check_duplicates(stack_a);
+	return (stack_a);
+}
+
+void	err_func(void)
+{
+	write(2, "Error\n", 6);
+	exit(0);
+}
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,94 +6,32 @@
 /*   By: ivork <ivork@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/20 15:39:00 by ivork         #+#    #+#                 */
-/*   Updated: 2021/10/12 21:42:10 by ivork         ########   odam.nl         */
+/*   Updated: 2021/10/13 17:37:08 by ivork         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "../includes/push_swap.h"
-#include "../libft/libft.h"
-#include <stdio.h>
-
-int	check_argument(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (!str[i])
-		return (0);
-	if (str[i] == '-' && str[i + 1] != '\0')
-		i++;
-	while (*str == '0')
-		str++;
-	while (str[i] != '\0')
-	{
-		if (!ft_isdigit(str[i]))
-			return (0);
-		i++;
-	}
-	if (i > 9 && ft_atoi(str) == 0)
-		return (0);
-	return (1);
-}
-
-t_stack	*create_new_node(void)
-{
-	t_stack	*first;
-
-	first = (t_stack *)malloc(sizeof(t_stack));
-	first->next = NULL;
-	return (first);
-}
-
-t_stack	*fill_stack(int argc, char **argv, t_stack *stack_a)
-{
-	t_stack	*tmp;
-	int		i;
-
-	i = 1;
-	tmp = stack_a;
-	while (i < argc)
-	{
-		if (!check_argument(argv[i]))
-			return (NULL);
-		tmp->num = ft_atoi(argv[i]);
-		tmp->next = NULL;
-		if (i < argc - 1)
-		{
-			tmp->next = create_new_node();
-			tmp = tmp->next;
-		}
-		i++;
-	}
-	return (stack_a);
-}
 
 int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
-
+	
 	if (argc < 2)
-	{
-		write(2, "Error\n", 6);
-		return (0);
-	}
+		err_func();
 	stack_a = create_new_node();
 	if (stack_a == NULL)
-		return (0);
+		err_func();
 	stack_a = fill_stack(argc, argv, stack_a);
 	if (stack_a == NULL)
-	{
-		write(2, "Error\n", 6);
-		return (0);
-	}
+		err_func();
 	if (is_sorted(stack_a))
 		return (0);
 	if (argc <= 6)
 		sort_short(&stack_a, (argc - 1));
 	else
 	{
-		sort_long(&stack_a, (argc - 1));
+		sort_long(&stack_a, (argc - 1), (argc / 25));
 	}
 	return (0);
 }
